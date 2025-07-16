e1cf754846ab3b569ca2d28f3984e5c0c9a9cc1f
Teri <klein.tereza@seznam.cz>
2025-07-16 16:09:30 +0200

    Page object model for Login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,52 +1,53 @@
 import {expect, test} from "@playwright/test";
 import {urls} from "../model/login.constants";
+import {LoginPage} from "../pages/login.page";
 
 test.describe("Login suite", () => {
     test("Standard user valid login", async ({page}) => {
-        await page.goto(urls.index);
-        await page.fill("#user-name", "standard_user");
-        await page.fill("#password", "secret_sauce");
-        await page.click("#login-button");
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login("standard_user", "secret_sauce");
+
         await expect(page).toHaveURL(urls.inventory);
     });
 
     test("Standard user login with invalid username", async ({page}) => {
-        await page.goto(urls.index);
-        await page.fill("#user-name", "standarduser");
-        await page.fill("#password", "secret_sauce");
-        await page.click("#login-button");
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login("standarduser", "secret_sauce");
+
         const errorAlert = await page.locator('h3[data-test="error"]');
         await expect(errorAlert).toHaveText('Epic sadface: Username and password do not match any user in this service');
     });
 
     test("Standard user login with invalid password", async ({page}) => {
-        await page.goto(urls.index);
-        await page.fill("#user-name", "standard_user");
-        await page.fill("#password", "secretsauce");
-        await page.click("#login-button");
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login("standard_user", "secretsauce");
+
         const errorAlert = await page.locator('h3[data-test="error"]');
         await expect(errorAlert).toHaveText('Epic sadface: Username and password do not match any user in this service');
     });
 
     test("Standard user login with empty username", async ({page}) => {
-        await page.goto(urls.index);
-        await page.fill("#user-name", "");
-        await page.fill("#password", "secret_sauce");
-        await page.click("#login-button");
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login("", "secret_sauce");
+
         const errorAlert = await page.locator('h3[data-test="error"]');
         await expect(errorAlert).toHaveText('Epic sadface: Username is required');
     });
 
     test("Standard user login with empty password", async ({page}) => {
-        await page.goto(urls.index);
-        await page.fill("#user-name", "standard_user");
-        await page.fill("#password", "");
-        await page.click("#login-button");
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login("standard_user", "");
+
         const errorAlert = await page.locator('h3[data-test="error"]');
         await expect(errorAlert).toHaveText('Epic sadface: Password is required');
     });
 
-    test("Password input should be hidden)", async ({ page }) => {
+    test("Password input should be hidden)", async ({page}) => {
         await page.goto(urls.index);
         const passwordField = page.locator("#password");
         // Check that the type is "password"
