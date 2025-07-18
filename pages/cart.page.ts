e934d209a923f49cc2870246aa434961a3df6562
Teri <klein.tereza@seznam.cz>
2025-07-18 13:58:35 +0200

    Cart Page Object Model

---
+++ pages/cart.page.ts
@@ -0,0 +1,77 @@
+import {expect, Page} from "@playwright/test";
+
+export class CartPageObjectModel {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    async addItemToCart(itemName: string, expectedItemCount: string) {
+        await this.page.click(`#add-to-cart-${itemName}`);
+
+        const cartBadge = this.page.locator(".shopping_cart_link");
+        await expect(cartBadge).toHaveText(expectedItemCount);
+    }
+
+    async removeItemFromCart(itemName: string, expectedItemCount: string) {
+        await this.page.click(`#remove-${itemName}`);
+
+        const cartBadge = this.page.locator(".shopping_cart_link");
+        await expect(cartBadge).toHaveText(expectedItemCount);
+    }
+
+    async addToRemove(itemName: string) {
+        await this.page.click(`#add-to-cart-${itemName}`);
+
+        const addButton = this.page.locator(`#remove-${itemName}`);
+        await expect(addButton).toHaveText("Remove");
+    }
+
+    async removeToAdd(itemName: string) {
+        await this.page.click(`#remove-${itemName}`);
+
+        const removeButton = this.page.locator(`#add-to-cart-${itemName}`);
+        await expect(removeButton).toHaveText("Add to cart");
+    }
+
+    async sortZA() {
+        await this.page.selectOption(".product_sort_container", "Name (Z to A)");
+
+        const item3 = this.page.locator("#item_3_title_link");
+        await expect(item3).toHaveText("Test.allTheThings() T-Shirt (Red)");
+    }
+
+    async sortAZ() {
+        await this.page.selectOption(".product_sort_container", "Name (A to Z)");
+
+        const item4 = this.page.locator("#item_4_title_link");
+        await expect(item4).toHaveText("Sauce Labs Backpack");
+    }
+
+    async sortPriceLowToHigh() {
+        await this.page.selectOption(".product_sort_container", "Price (low to high)");
+
+        const itemPrice = this.page.locator(".inventory_item_price").locator("nth=0");
+        await expect(itemPrice).toHaveText("$7.99");
+    }
+
+    async sortPriceHighToLow() {
+        await this.page.selectOption(".product_sort_container", "Price (high to low)");
+
+        const itemPrice = this.page.locator(".inventory_item_price").locator("nth=0");
+        await expect(itemPrice).toHaveText("$49.99");
+    }
+
+    async itemNameNavigation(itemName: string, itemNumber: number) {
+        await this.page.click(`#${itemName}_title_link`);
+
+        await expect(this.page).toHaveURL(`https://www.saucedemo.com/inventory-item.html?id=${itemNumber}`);
+    }
+
+    async itemImageNavigation(itemName: string, itemNumber: number) {
+        await this.page.click(`#${itemName}_img_link`);
+
+        await expect(this.page).toHaveURL(`https://www.saucedemo.com/inventory-item.html?id=${itemNumber}`);
+    }
+}
--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,57 +1,88 @@
-import {expect, test} from "@playwright/test";
+import {test} from "@playwright/test";
 import {LoginPage} from "../pages/login.page";
+import {CartPageObjectModel} from "../pages/cart.page";
 
 test.describe('Cart suite', () => {
     test("Add to cart button adds product to cart", async ({page}) => {
         const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
 
         await loginPage.login("standard_user", "secret_sauce");
-
-        await page.click("#add-to-cart-sauce-labs-backpack");
-
-        const cartBadge = page.locator(".shopping_cart_link");
-        await expect(cartBadge).toHaveText("1");
+        await cartPage.addItemToCart("sauce-labs-backpack", "1")
     })
 
     test("Remove button removes product from cart", async ({page}) => {
         const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
 
         await loginPage.login("standard_user", "secret_sauce");
-
-        await page.click("#add-to-cart-sauce-labs-backpack");
-
-        const cartBadge = page.locator(".shopping_cart_link");
-        await expect(cartBadge).toHaveText("1");
-
-        await page.click("#remove-sauce-labs-backpack");
-
-        await expect(cartBadge).toHaveText("");
+        await cartPage.addItemToCart("sauce-labs-backpack", "1")
+        await cartPage.removeItemFromCart("sauce-labs-backpack", "")
     })
 
     test("Add to cart button changes to Remove", async ({page}) => {
         const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
 
         await loginPage.login("standard_user", "secret_sauce");
-
-        await page.click("#add-to-cart-sauce-labs-backpack");
-
-        const addButton = page.locator("#remove-sauce-labs-backpack");
-        await expect(addButton).toHaveText("Remove");
+        await cartPage.addToRemove("sauce-labs-backpack")
     })
 
     test("Remove button changes to Add to cart", async ({page}) => {
         const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
 
         await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.addToRemove("sauce-labs-backpack")
+        await cartPage.removeToAdd("sauce-labs-backpack")
+    })
 
-        await page.click("#add-to-cart-sauce-labs-backpack");
+    test("Sort items by name Z-A", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
 
-        const addButton = page.locator("#remove-sauce-labs-backpack");
-        await expect(addButton).toHaveText("Remove");
+        await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.sortZA()
+    })
 
-        await page.click("#remove-sauce-labs-backpack");
+    test("Sort items by name A-Z", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
 
-        const removeButton = page.locator("#add-to-cart-sauce-labs-backpack");
-        await expect(removeButton).toHaveText("Add to cart");
+        await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.sortZA()
+        await cartPage.sortAZ()
+    })
+
+    test("Sort items by price (low to high)", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
+
+        await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.sortPriceLowToHigh()
+    })
+
+    test("Sort items by price (high to low)", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
+
+        await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.sortPriceHighToLow()
+    })
+
+    test("Click on product name navigates to product detail page", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
+
+        await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.itemNameNavigation("item_4", "4")
+    })
+
+    test("Click on product image navigates to product detail page", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        const cartPage = new CartPageObjectModel(page);
+
+        await loginPage.login("standard_user", "secret_sauce");
+        await cartPage.itemImageNavigation("item_4", "4")
     })
 });
